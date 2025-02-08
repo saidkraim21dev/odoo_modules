a602b7a35451359ccb151f123dd9df17fe43f03a
said.kraim <said.kraim>
2025-02-08 16:57:54 +0100

    Add new custom module of data country state + city for algeria

---
+++ l10n_dz_states/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2025 Said Kraim
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "States and city for Algeria",
+    "category": "Localisation",
+    "summary": """
+        This module extends res.country.states and res.city for Algeria.
+    """,
+    "version": "18.0.1.0.0",
+    "author": "Said Kraim",
+    "website": "",
+    "license": "AGPL-3",
+    "depends": ['base', 'base_address_extended'],
+    "data": [
+        "data/res_country_states.xml",
+        "data/res_city.xml",
+    ],
+    'price': 00,
+    'currency': 'EUR',
+    "installable": True,
+    "application": False,
+}
